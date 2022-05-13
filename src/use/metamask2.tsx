b997b603a757d45d26c2ae573e05053f042812fb
zlumer <zlumer@gmail.com>
2022-05-13 12:55:54 +0300

    useMetamask fix

--- src/use/metamask2.tsx
+++ src/use/metamask2.tsx
@@ -1,16 +1,13 @@
 import React from "react"
+import { useMetamask } from "use-metamask"
 
 // react HOC that returns a placeholder if user has not connected metamask
 export const withConnectedMetamask = <T extends {}>(Component: React.FC<T>, Placeholder: React.FC): React.FC<T> => (props: T) => {
-	// const metamask = useMetamask()
-	// return metamask.metaState.isConnected ? <Component {...props} /> : <Placeholder />
-
-	return <></>
+	const metamask = useMetamask()
+	return metamask.metaState.isConnected ? <Component {...props} /> : <Placeholder />
 }
 
 export const withInstalledMetamask = <T extends {}>(Component: React.FC<T>, Placeholder: React.FC): React.FC<T> => (props: T) => {
-	// const metamask = useMetamask()
-	// return metamask.metaState.isAvailable ? <Component {...props} /> : <Placeholder />
-
-	return <></>
+	const metamask = useMetamask()
+	return metamask.metaState.isAvailable ? <Component {...props} /> : <Placeholder />
 }
