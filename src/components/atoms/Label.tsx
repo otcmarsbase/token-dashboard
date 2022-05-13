0f8fe02d4729360bf4cfe700d07055938994f22a
Alex5 <angrymarshall@gmail.com>
2022-05-13 12:47:54 +0300

    fix label types

--- src/components/atoms/Label.tsx
+++ src/components/atoms/Label.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode} from "react"
 import PlasmicLabel, {LabelProps} from '../Label';
 
-export type TagLabelColors =  "purple" | "cyan" | "green"
+export type TagLabelColors =  "purple" | "cyan" | "green" | 'yellow'
 
 export const Label: React.FC<{ children: ReactNode } & LabelProps> = ({children, ...props}) => {
     return <PlasmicLabel {...props}>{children}</PlasmicLabel>
