9ebbe1b91bce6c5932c50e6cedf38dd825dd39d6
zlumer <zlumer@gmail.com>
2022-05-13 12:59:04 +0300

    state draft

--- src/contexts/AppStateContext.tsx
+++ src/contexts/AppStateContext.tsx
@@ -1,21 +1,26 @@
-import React, {ReactNode} from "react"
+import React, { ReactNode } from "react"
+import { createApi } from "../api"
 import { SCREEN_DATA, HANDLERS } from "../data"
 
 export const AppStateContext = React.createContext({
-  data: SCREEN_DATA,
-  handlers: HANDLERS
+	data: SCREEN_DATA,
+	handlers: HANDLERS
 })
 
-export const AppStateProvider: React.FC<{children: ReactNode}> = props =>
+export const AppStateProvider: React.FC<{ children: ReactNode }> = props =>
 {
-  let state = React.useMemo(() => ({
-    data: SCREEN_DATA,
-    handlers: HANDLERS,
-  }), [])
+	let state = React.useMemo(() => ({
+		data: SCREEN_DATA,
+		handlers: HANDLERS,
+	}), [])
 
-  return (
-    <AppStateContext.Provider value={state}>
-      {props.children}
-    </AppStateContext.Provider>
-  )
+	let api = React.useMemo(() => createApi("0xCcb684ACA4068D75692f3414328A65A65BBc2A09", "0xcFE1A80bc0de6723c955fB496520cEF3f52072C0", window.ethereum), [window.ethereum])
+	state.handlers.onClaim = api.requestClaimOneSignature
+	// state.handlers.onClaimAll = api.requestClaimAllSignature
+
+	return (
+		<AppStateContext.Provider value={state}>
+			{props.children}
+		</AppStateContext.Provider>
+	)
 }
