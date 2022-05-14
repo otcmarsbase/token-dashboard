036ba3fd9691c8a93fb9036cd9499e60a910d2cc
Alex5 <angrymarshall@gmail.com>
2022-05-14 18:19:16 +0300

    forwarded signer provider

--- src/api.ts
+++ src/api.ts
@@ -1,62 +1,64 @@
-import { BigNumber, BigNumberish, ethers } from "ethers"
-import { INft } from "./components/organisms/NftTable"
-import { MarsbaseVesting__factory, MarsbaseVesting } from "@otcmarsbase/token-contract-types"
+import {BigNumber, BigNumberish, ethers} from "ethers"
+import {INft} from "./components/organisms/NftTable"
+import {MarsbaseVesting__factory, MarsbaseVesting} from "@otcmarsbase/token-contract-types"
 
 type NftData = Awaited<ReturnType<MarsbaseVesting["getVestingRecord"]>>
 
 
-export function createApi(mbaseAddress: string, vestingAddress: string, provider: ethers.Signer | ethers.providers.Provider)
-{
-	let vest = MarsbaseVesting__factory.connect(vestingAddress, provider)
-	return {
-		getNftList: (address: string) => getNftList(address, vest),
-		requestClaimOneSignature: (nftId: string) => requestClaimOneSignature(nftId, vest),
-		requestClaimAllSignature: (nfts: INft[]) => requestClaimAllSignature(nfts, vest)
-	}
+export function createApi(mbaseAddress: string, vestingAddress: string, provider: ethers.Signer | ethers.providers.Provider) {
+    let vest = MarsbaseVesting__factory.connect(vestingAddress, provider)
+    return {
+        getNftList: (address: string) => getNftList(address, vest),
+        requestClaimOneSignature: (nftId: string) => requestClaimOneSignature(nftId, vest),
+        requestClaimAllSignature: (nfts: INft[]) => requestClaimAllSignature(nfts, vest)
+    }
 }
 
-export async function requestMetamaskConnect(): Promise<string | undefined>
-{
-	if (!window.ethereum)
-	{
-		console.log('MetaMask is not installed!')
-		return
-	}
+export async function requestMetamaskConnect(): Promise<string | undefined> {
+    if (!window.ethereum) {
+        console.log('MetaMask is not installed!')
+        return
+    }
 
-	const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
 
-	return accounts[0]
+    return accounts[0]
 }
-export async function getNftList(address: string, vest: MarsbaseVesting)
-{
-	const vestingsIds = await vest.getVestingsByOwner(address)
-	const records = await Promise.all(vestingsIds.map((x: BigNumberish) => vest.getVestingRecord(x)))
-	return records
+
+export async function getNftList(address: string, vest: MarsbaseVesting) {
+    const vestingsIds = await vest.getVestingsByOwner(address)
+    const records = await Promise.all(vestingsIds.map((x: BigNumberish) => vest.getVestingRecord(x)))
+    return records
 }
-export function calculateClaimableTokens(nft: NftData): BigNumber
-{
-	let start = nft.start.toNumber()
-	let end = nft.end.toNumber()
-	let now = Date.now() / 1000
-	let vestingDuration = end - start
-	let elapsed = Math.min(now - start, vestingDuration)
-	let percent = elapsed / vestingDuration
-	let claimable = nft.amount.mul(percent)
-	return claimable
+
+export function calculateClaimableTokens(nft: NftData): BigNumber {
+    let start = nft.start.toNumber()
+    let end = nft.end.toNumber()
+    let now = Date.now() / 1000
+    let vestingDuration = end - start
+    let elapsed = Math.min(now - start, vestingDuration)
+    let percent = elapsed / vestingDuration
+    let claimable = nft.amount.mul(percent)
+    return claimable
 }
-export async function requestClaimOneSignature(nftId: string, vest: MarsbaseVesting): Promise<boolean>
-{
-	const tx = await vest["unvest(uint256)"](nftId)
 
-	await tx.wait()
+export async function requestClaimOneSignature(nftId: string, vest: MarsbaseVesting): Promise<boolean> {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const address = await requestMetamaskConnect();
+    const signer = provider.getSigner(address);
+    const signerAddress = await signer.getAddress();
 
-	return true
+    const tx = await vest["unvest(uint256)"](nftId)
+
+    await tx.wait()
+
+    return true
 }
-export async function requestClaimAllSignature(nfts: INft[], vest: MarsbaseVesting): Promise<boolean>
-{
-	const tx = await vest["unvest(uint256[])"](nfts.map(x => x.id))
 
-	await tx.wait()
+export async function requestClaimAllSignature(nfts: INft[], vest: MarsbaseVesting): Promise<boolean> {
+    const tx = await vest["unvest(uint256[])"](nfts.map(x => x.id))
 
-	return true
+    await tx.wait()
+
+    return true
 }
--- src/contexts/AppStateContext.tsx
+++ src/contexts/AppStateContext.tsx
@@ -1,26 +1,46 @@
-import React, { ReactNode } from "react"
-import { createApi } from "../api"
-import { SCREEN_DATA, HANDLERS } from "../data"
+import React, {ReactNode, useEffect, useState} from "react"
+import {createApi} from "../api"
+import {SCREEN_DATA, HANDLERS} from "../data"
+import {ethers, Signer} from "ethers";
 
 export const AppStateContext = React.createContext({
-	data: SCREEN_DATA,
-	handlers: HANDLERS
+    data: SCREEN_DATA,
+    handlers: HANDLERS
 })
 
-export const AppStateProvider: React.FC<{ children: ReactNode }> = props =>
-{
-	let state = React.useMemo(() => ({
-		data: SCREEN_DATA,
-		handlers: HANDLERS,
-	}), [])
+export const AppStateProvider: React.FC<{ children: ReactNode }> = props => {
+    let state = React.useMemo(() => ({
+        data: SCREEN_DATA,
+        handlers: HANDLERS,
+    }), [])
 
-	let api = React.useMemo(() => createApi("0xCcb684ACA4068D75692f3414328A65A65BBc2A09", "0xcFE1A80bc0de6723c955fB496520cEF3f52072C0", window.ethereum), [window.ethereum])
-	state.handlers.onClaim = api.requestClaimOneSignature
-	// state.handlers.onClaimAll = api.requestClaimAllSignature
+    const [provider, setProvider] = useState<Signer | ethers.providers.Provider>();
+    const getProvider = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signerProvider = await provider.send("eth_requestAccounts", []);
+        if (!signerProvider) {
+            return provider;
+        }
+        return provider.getSigner();
+    }
 
-	return (
-		<AppStateContext.Provider value={state}>
-			{props.children}
-		</AppStateContext.Provider>
-	)
+    useEffect(() => {
+        getProvider().then(provider => setProvider(provider));
+    }, [])
+
+    let api = React.useMemo(() => {
+        return createApi(
+            "0xCcb684ACA4068D75692f3414328A65A65BBc2A09",
+            "0xcFE1A80bc0de6723c955fB496520cEF3f52072C0",
+            provider!)
+    }, [provider])
+
+    state.handlers.onClaim = api.requestClaimOneSignature
+    // state.handlers.onClaimAll = api.requestClaimAllSignature
+
+    return (
+        <AppStateContext.Provider value={state}>
+            {props.children}
+        </AppStateContext.Provider>
+    )
 }
