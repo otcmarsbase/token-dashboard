4249f531cc8e8bf83eb3d9cd4ed51b731bca6b86
kirill pushkin <k1rilllavrinenko@yandex.ru>
2022-05-24 00:51:35 +0300

    fixed error on async await and started metamask auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 
 import TokenDashboard from "./components/pages/TokenDashboard"
+import 'regenerator-runtime'
 
-import { AppStateProvider } from "./contexts/AppStateContext"
+import { AppStateContext, AppStateProvider } from "./contexts/AppStateContext"
 import { DictionaryProvider } from "./contexts/DictionaryContext"
-import { MetaMaskProvider } from "metamask-react"
+import { MetaMaskProvider, useMetaMask } from "metamask-react"
 
 import "./styles.css"
+import { useMetamask } from "use-metamask"
 
 declare global {
 	interface Window {
@@ -14,12 +16,30 @@ declare global {
 	}
 }
 
+/* сейчас проблема в том, что изменение статуса метамаска не ререндерит компонент, из за этого после подключения надо обновлять страницу, в следующем пуше я это поправлю */
+
+const Display: React.FC = props => { /* Template test component for metamask authentication */
+	const { status } = useMetaMask()
+
+	const [render, setRender] = useState(<div>Connect meta</div>)
+
+	useEffect(() => {
+		if (status == 'connected')
+			setRender(<TokenDashboard />)
+	})
+	 
+	return (
+		render
+	)
+}
+
 export default function App() {
 	return (
 		<DictionaryProvider lang="en">
 			<AppStateProvider>
 				<MetaMaskProvider>
-					<TokenDashboard />
+					{/* <TokenDashboard /> */}
+					<Display />
 				</MetaMaskProvider>
 			</AppStateProvider>
 		</DictionaryProvider>
