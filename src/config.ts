6243c383374784dd85c0c7004f9f5000b6420c7a
zlumer <zlumer@gmail.com>
2022-06-13 10:53:17 +0300

    new contracts (0.1.2)

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const NETWORKS = ([
 		name: "Marsbase",
 		chainId: 1337,
 		rpcUrl: "https://happy-heron-57.deno.dev",
-		mbaseTokenAddress: "0x605cc9e1C86A8dC0cFe7303D06f2889a4491906C",
-		mbaseVestingAddress: "0x728f79949a66c9950C39A8FD0BF9d353655d689C",
+		mbaseTokenAddress: "0x765Eb9A1518C2906D11D330a11dBA294a7673E84",
+		mbaseVestingAddress: "0xd37A38DC1BE88c33033821025A7CC5664d2b7328",
 		blockExplorerUrl: "https://134.122.65.135/",
 	},
 	{
